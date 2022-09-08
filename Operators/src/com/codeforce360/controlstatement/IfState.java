205e94d0bc831eba9ce338ba23c0a5c2a39a0d92
mlbgayu <mlbgayu@gmail.com>
2022-09-08 12:37:23 -0400

    added switch

---
+++ Operators/src/com/codeforce360/controlstatement/IfState.java
@@ -0,0 +1,28 @@
+package com.codeforce360.controlstatement;
+
+public class IfState {
+
+	public static void main(String[] args) {
+		int mark=90;
+		if (mark>70){
+			System.out.println("Grade A");
+		}		
+		else if(mark>80) {
+		
+			System.out.println("Grade B");
+		}
+		else if (mark>90)
+		{
+			System.out.println("Grade C");
+		}
+		else {
+		  
+			System.out.println("Fail");
+			
+		}
+	}
+}
+	
+	
+
+

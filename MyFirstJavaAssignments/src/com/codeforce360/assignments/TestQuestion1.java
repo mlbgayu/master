a907c2aa57c663f9fc9d4f5ecd6e6c2390ad782e
mlbgayu <mlbgayu@gmail.com>
2022-09-11 19:20:17 -0400

    Java Assignments

---
+++ MyFirstJavaAssignments/src/com/codeforce360/assignments/TestQuestion1.java
@@ -0,0 +1,16 @@
+package com.codeforce360.assignments;
+
+public class TestQuestion1 {
+
+	public static void main(String[] args) {
+
+		//for (int i = 5; i >= 1; i--) {
+		for (int i = 1; i <= 5; i++) {
+
+			for (int j = 1; j <= 5; ++j) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}

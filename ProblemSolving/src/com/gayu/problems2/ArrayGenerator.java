cdebd0742aa39657794b34dfead685fbd8ca0086
mlbgayu <mlbgayu@gmail.com>
2022-09-25 21:38:29 -0400

    Generate Array

---
+++ ProblemSolving/src/com/gayu/problems2/ArrayGenerator.java
@@ -0,0 +1,31 @@
+package com.gayu.problems2;
+
+import java.util.Arrays;
+/*
+ * 
+ * 6. Write a function that, given the start startNum and end endNum values, return an array containing all the numbers inclusive to that range. See examples below.
+ 
+Examples - 
+
+inclusiveArray(1, 5) ➞ [1, 2, 3, 4, 5] 
+inclusiveArray(2, 8) ➞ [2, 3, 4, 5, 6, 7, 8]
+
+@author Gayathri
+ * */
+
+public class ArrayGenerator {
+
+	int[] inclusiveArray(int a, int b) {
+		int gen[] = new int[(b - 1)];
+		for (int i = 0; i < gen.length; i++) {
+			gen[i] = a++;
+		}
+		return gen;
+	}
+
+	public static void main(String[] args) {
+		ArrayGenerator gen = new ArrayGenerator();
+		int arr[] = gen.inclusiveArray(2, 8);
+		System.out.println(Arrays.toString(arr));
+	}
+}

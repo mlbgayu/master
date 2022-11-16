a0f815afc71f330fa68fd2ce2ebd6545478acaec
mlbgayu <mlbgayu@gmail.com>
2022-11-16 09:10:27 -0500

    Hybernate

---
+++ ProjectGUI/HibernetMaven/src/main/java/com/codeforce360/HibernetMaven/App.java
@@ -0,0 +1,13 @@
+package com.codeforce360.HibernetMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
